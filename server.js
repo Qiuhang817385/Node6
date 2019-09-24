c8f26ea02c69dcfb8bf9477385f0605e904c5e59
Qiuhang817385 <826266129@qq.com>
2019-09-24 21:32:50 +0800

    none

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
  //加载一个http模块，目的是创建使用这个对象创建一个Web服务器
- const http = require('http'); const hostname = '127.0.0.1'; 
+ const http = require('http'); 
+ const hostname = '127.0.0.1'; 
  const port = 3000;
  const server = http.createServer((req, res) => { 
     res.statusCode = 200; 
-    res.setHeader('Content-Type', 'text/plain'); res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/plain'); 
+    res.end('Hello World\n');
   });
  server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`); 
